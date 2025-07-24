0aa33d7c17fb15627e351b1fdf591b39a514fbe2
bennymei211 <bennymei211@gmail.com>
2025-07-23 20:56:47 -0400

    Update image_generation.py

--- tests/image_generation.py
+++ tests/image_generation.py
@@ -1,9 +1,11 @@
 from openai import OpenAI
 from dotenv import load_dotenv
+import base64
 import os
 # from keys import openai_api_key
 
-client = OpenAI(api_key="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")
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 # prompt
 user_input = "Generate an image of gray tabby cat hugging an otter with an orange scarf"
@@ -11,3 +13,5 @@ user_input = "Generate an image of gray tabby cat hugging an otter with an orang
 response = client.images.generate(model='dall-e-3', prompt=user_input, size='1024x1024', quality='standard', n=1, style='vivid')
 image_url = response.data[0].url
 print(image_url)
+
+
